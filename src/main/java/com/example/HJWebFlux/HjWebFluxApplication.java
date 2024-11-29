de40cbe2f2091f6528439e0a89009e8b7536a10f
o0780 <aboutlim@gmail.com>
2024-11-29 10:46:50 +0900

    Initial commit

---
+++ src/main/java/com/example/HJWebFlux/HjWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.HJWebFlux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HjWebFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HJWebFlux/HjWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HJWebFlux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
